97282a55b1907827578c267f929ce234daf5ad05
cozenakai <masaki2970@icloud.com>
2020-04-20 23:21:44 +0900

    finish

--- pages/index.vue
+++ pages/index.vue
@@ -49,13 +49,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  font-family: 'Permanent Marker', cursive;
+  
 }
 
 .subtitle {
